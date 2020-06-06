d857945f009f5cd462f70ae69c9e147626fedf43
prajwal-vr <prajwal-vr@users.noreply.github.com>
2020-06-06 21:39:05 +0530

    Code file created.

---
+++ Visualization-for-company-stakeholders/code.py
@@ -0,0 +1,111 @@
+# --------------
+#Importing header files
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=pd.read_csv(path)
+loan_status=data['Loan_Status'].value_counts()
+plt.figure(figsize=[14,8])
+
+# label the axes
+plt.xlabel("Type")
+plt.ylabel("Status")
+
+# title the plot
+plt.title("Loan Status")
+
+# build and show the plot
+plt.bar(loan_status.index,loan_status)
+plt.show()
+
+
+#Code starts here
+
+
+# --------------
+#Code starts here
+
+
+
+property_and_loan=data.groupby(['Property_Area','Loan_Status']).size().unstack()
+property_and_loan.plot(kind='bar', stacked=False, figsize=(15,10))
+plt.xlabel('Property Area')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+
+
+
+education_and_loan=data.groupby(['Education','Loan_Status']).size().unstack()
+education_and_loan.plot(kind='bar',stacked=True,figsize=(12,12))
+plt.xlabel('Education Status')
+plt.ylabel('Loan Status')
+plt.xticks(rotation=45)
+plt.show()
+
+
+# --------------
+#Code starts here
+graduate=data[data['Education'] == 'Graduate']
+not_graduate=data[data['Education'] == 'Not Graduate']
+graduate['LoanAmount'].plot(kind='density', label='Graduate',figsize=(12,12))
+not_graduate['LoanAmount'].plot(kind='density', label='Not Graduate',figsize=(12,12))
+
+plt.show()
+
+
+
+
+
+
+
+#Code ends here
+
+#For automatic legend display
+plt.legend()
+
+
+# --------------
+#Code starts here
+fig ,(ax_1,ax_2,ax_3)=plt.subplots(nrows = 3 , ncols = 1)
+data.plot.scatter(x='ApplicantIncome',y='LoanAmount',ax=ax_1)
+ax_1.set_title('Applicant Income')
+data.plot.scatter(x='CoapplicantIncome',y='LoanAmount',ax=ax_2)
+ax_2.set_title('Coapplicant Income')
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+data.plot.scatter(x='TotalIncome',y='LoanAmount',ax=ax_3)
+ax_3.set_title('Total Income')
+plt.show()
+
+fig, (ax_1, ax_2,ax_3) = plt.subplots(1,3, figsize=(20,8))
+
+#Plotting scatter plot
+ax_1.scatter(data['ApplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_1.set(title='Applicant Income')
+
+
+#Plotting scatter plot
+ax_2.scatter(data['CoapplicantIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_2.set(title='Coapplicant Income')
+
+
+#Creating a new column 'TotalIncome'
+data['TotalIncome']= data['ApplicantIncome']+ data['CoapplicantIncome']
+
+#Plotting scatter plot
+ax_3.scatter(data['TotalIncome'],data["LoanAmount"])
+
+#Setting the subplot axis title
+ax_3.set(title='Total Income')
+plt.show()
+
+
